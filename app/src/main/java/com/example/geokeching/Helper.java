24ba9b810ac72b46dcc3e9935b21090fc16ce282
Valeria Shulgina <valeronfor@mail.ru>
2023-04-13 21:25:08 +0300

    new commit

---
+++ app/src/main/java/com/example/geokeching/Helper.java
@@ -0,0 +1,4 @@
+package com.example.geokeching;
+
+public class Helper {
+}
--- app/src/main/java/com/example/geokeching/Map.java
+++ app/src/main/java/com/example/geokeching/Map.java
@@ -2,9 +2,11 @@ package com.example.geokeching;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.GeoObjectCollection;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
@@ -16,16 +18,25 @@ public class Map extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        MapKitFactory.setApiKey("6606b5ff-4e85-43c8-8471-fb36f7c87329");
+
 
         MapKitFactory.initialize(this);
 
         // Укажите имя Activity вместо map.
         setContentView(R.layout.activity_map);
         mapview = (MapView)findViewById(R.id.mapview);
-        mapview.getMap().move(
+        com.yandex.mapkit.map.Map map = mapview.getMap();
+        map.move(
                 new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 0),
                 null);
+        map.getMapObjects().addPlacemark(new Point(55.75222, 37.61556));
+
+        //добавление слоя дорог и улиц
+        GeoObjectCollection geoObjects = new GeoObjectCollection();
+
+
     }
+
+
 }
